63f296f117f13f499ab17168fc6b6ca9249eb422
TacoOtter123 <39072759+TacoOtter123@users.noreply.github.com>
2019-02-19 10:10:51 -0800

    Create team16.py

---
+++ team16.py
@@ -0,0 +1,8 @@
+def move(my_history, their_history, my_score, their_score):
+    if len(my_history) == 0:
+      return 'b'
+    else:
+      if their_history[len(their_history)-1] == 'b':
+        return 'b'
+      else:
+        return 'c'
