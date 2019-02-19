0e4628da9939716d4f78cb8afb650e9000e1f289
legion7274 <47792364+legion7274@users.noreply.github.com>
2019-02-19 10:29:09 -0800

    Delete team19.py

--- team19.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def move(my_history, their_history, my_score, their_score):
-    if len(my_history) == 0:
-        return 'b'
-    else:
-        choices = ['c', 'c', 'c', 'b', 'b', 'b', 'c', 'b', 'c', 'b', 'b']
-        return random.choice(choices)
